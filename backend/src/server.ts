cedb888b0961f8e38959710c684cf909f7510374
Stussyz <mfadhol07@gmail.com>
2026-04-24 23:29:25 +0700

    add routes products

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import cors from "cors";
+import productRoutes from "./routes/productRoutes";
 
 const app = express();
 const PORT = 5000;
 
 app.use(cors());
 app.use(express.json());
+// add routes baru: /products
+app.use("/products", productRoutes)
 
 app.get("/", (req, res) => {
     res.send("Congrats!, your API is working fine!");
