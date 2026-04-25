bdd7ae1c7290f56c5797b486cb26c9dac5f2f061
Stussyz <mfadhol07@gmail.com>
2026-04-25 13:13:30 +0700

    data type of interface product

---
+++ frontend/src/types/product.ts
@@ -0,0 +1,10 @@
+export interface Product {
+    id: number;
+    category_id: number;
+    name: string;
+    price: number;
+    stock: number;
+    image_url: string;
+    is_active: boolean;
+    
+}
