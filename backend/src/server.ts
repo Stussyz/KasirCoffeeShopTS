c3b05ae762eb8719a4cb263d74c9570c2523b9f2
Stussyz <mfadhol07@gmail.com>
2026-04-30 19:02:32 +0700

    import transactionRoutes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,18 +1,22 @@
 import express from "express";
 import cors from "cors";
 import productRoutes from "./routes/productRoutes";
+import transactionRoutes from "./routes/transactionRoutes";
 
 const app = express();
 const PORT = 5000;
 
 app.use(cors());
 app.use(express.json());
-// add routes baru: /products
-app.use("/products", productRoutes)
 
 app.get("/", (req, res) => {
     res.send("Congrats!, your API is working fine!");
 });
+
+// add routes baru: /products
+app.use("/products", productRoutes)
+// add routes baru: /transactions
+app.use("/transactions", transactionRoutes)
 
 app.listen(PORT, () => {
     console.log(`Your server is running on http://localhost:${PORT}`);
