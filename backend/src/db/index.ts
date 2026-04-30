5bf04ff2027458496c36d98bcbd98f9d4efb0a09
Stussyz <mfadhol07@gmail.com>
2026-04-30 19:08:15 +0700

    add console.log to catch db user n name, add db connection status

--- backend/src/db/index.ts
+++ backend/src/db/index.ts
@@ -3,9 +3,16 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_NAME:", process.env.DB_NAME);
+
 export const db = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
 });
+
+db.getConnection()
+    .then(() => console.log("Database Terhubung"))
+    .catch((err) => console.error("Gagal terhubung ke database:", err));
