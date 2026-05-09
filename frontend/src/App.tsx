b84994c54d03ed3dc547b5239dac106241778131
Stussyz <mfadhol07@gmail.com>
2026-05-10 04:19:16 +0700

    update focus on routing link

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
 import CashierPage from "./pages/CashierPage";
+import TransactionsHistoryPage from "./pages/TransactionHistoryPage";
 
 function App() {
-  return <CashierPage />;
+  return (
+    <BrowserRouter>
+    <MainLayout>
+      <Routes>
+        <Route path="/" element={<CashierPage />} />
+        <Route path="/transactions" element={<TransactionsHistoryPage/>} />
+      </Routes>
+    </MainLayout>
+    </BrowserRouter>
+  );
 }
 
 export default App;
